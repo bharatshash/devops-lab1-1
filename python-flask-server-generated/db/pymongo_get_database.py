f6bfe59b5578d8cf38722232c8b2e5c3584f964a
bshashwat <shashwat.bharat@gmail.com>
2025-03-09 19:27:25 +0100

    postman folder

--- python-flask-server-generated/db/pymongo_get_database.py
+++ python-flask-server-generated/db/pymongo_get_database.py
@@ -5,10 +5,12 @@ from pymongo import MongoClient
 # MONGO_URI  = "mongodb://user:password@mongo:27017/?directConnection=true"
 
 
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo-studentdb-service/student_db")
+client = MongoClient(MONGO_URI)
 
 
 # client = MongoClient(MONGO_URI)
-client = MongoClient('localhost', 27017)
+# client = MongoClient('localhost', 27017)
 db = client['student_service']
 
 if 'students' not in db.list_collection_names():
